c0411888f9780995ca0ebadfb47883c958aeee26
Azhar9Data <azharalbarrak@gmail.com>
2024-09-26 11:40:51 +0300

    Initial Commit

---
+++ YourFirstXCode/YourFirstXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstXCodeApp.swift
+//  YourFirstXCode
+//
+//  Created by Azhar on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct YourFirstXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
